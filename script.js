8e65ccda6ad4c10dc1b1e26379da3c3f1412adb3
dria-2006 <octobor2006@gmail.com>
2025-02-17 01:06:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('loveButton').addEventListener('click', function() {
+    document.getElementById('loveMessage').innerText = "Пүрэвочирт би чамд хайртай!";
+    document.getElementById('loveMessage').style.display = "block";
+});
